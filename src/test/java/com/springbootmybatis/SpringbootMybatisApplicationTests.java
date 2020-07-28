a8ca1ddd13cc906828664aac218814f7e711f356
zk <447511881@qq.com>
2020-07-28 18:28:42 +0800

    jdk8 流

--- src/test/java/com/springbootmybatis/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -3,11 +3,38 @@ package com.springbootmybatis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootTest
 class SpringbootMybatisApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        List<String> a = new ArrayList<>();
+
+        String dataA[] = {"a", "b", "c"};
+
+        Collections.addAll(a, dataA);
+
+        List<String> b = new ArrayList<>();
+
+        String dataB[] = {"a", "d"};
+
+        Collections.addAll(b, dataB);
+
+        List<String> res = a.stream().filter(s -> !b.contains(s)).collect(Collectors.toList());
+
+        System.out.println(res);
+
     }
 
 }
