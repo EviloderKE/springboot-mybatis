39e70f9094010fb790e694eca4eec5c3302ee7f7
zk <447511881@qq.com>
2020-08-06 15:51:56 +0800

    easyexcel简单导出

--- src/main/java/com/springbootmybatis/controller/EasyExcelController.java
+++ src/main/java/com/springbootmybatis/controller/EasyExcelController.java
@@ -2,14 +2,19 @@ package com.springbootmybatis.controller;
 
 import com.alibaba.excel.EasyExcel;
 import com.springbootmybatis.Excel.UserExcelListener;
+import com.springbootmybatis.qo.ExportExcel;
 import com.springbootmybatis.qo.UserExcel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("easyexcel")
@@ -21,6 +26,29 @@ public class EasyExcelController {
         //EasyExcel.read(file.getInputStream(), UserExcel.class, new UserExcelListener()).sheet().headRowNumber(指定行).doRead();
         EasyExcel.read(file.getInputStream(), UserExcel.class, new UserExcelListener()).sheet().doRead();
         return "success";
+    }
+
+    @GetMapping("download")
+    public void download(HttpServletResponse response) throws IOException {
+        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName = URLEncoder.encode("测试", "UTF-8");
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+
+        ArrayList<ExportExcel> al = new ArrayList<>();
+        ExportExcel u1 = new ExportExcel();
+        u1.setAge(24);
+        u1.setName("zk");
+        al.add(u1);
+
+        ExportExcel u2 = new ExportExcel();
+        u2.setAge(20);
+        u2.setName("zk1");
+        al.add(u2);
+
+        EasyExcel.write(response.getOutputStream(), ExportExcel.class).sheet("模板").doWrite(al);
     }
 
 }
---
+++ src/main/java/com/springbootmybatis/qo/ExportExcel.java
@@ -0,0 +1,12 @@
+package com.springbootmybatis.qo;
+
+import lombok.Data;
+
+@Data
+public class ExportExcel {
+
+    private String name;
+
+    private Integer age;
+
+}
