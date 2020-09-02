1fc38945176b8b706ac8855ab60fc484679442f3
zk <447511881@qq.com>
2020-09-02 15:48:29 +0800

    rabbitmq 直接发送到queue 定时任务生产

--- src/main/java/com/springbootmybatis/SpringbootMybatisApplication.java
+++ src/main/java/com/springbootmybatis/SpringbootMybatisApplication.java
@@ -1,20 +1,16 @@
 package com.springbootmybatis;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
-import javax.sql.DataSource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.springbootmybatis.dao")
 /**
  * 使用mapperscan或者在mapper上写@Mapper
  */
+@EnableScheduling
 public class SpringbootMybatisApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/springbootmybatis/listen/Tut1Receiver.java
@@ -0,0 +1,16 @@
+package com.springbootmybatis.listen;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "hello")
+public class Tut1Receiver {
+
+    @RabbitHandler
+    public void receive(String in) {
+        System.out.println(" [x] Received '" + in + "'");
+    }
+
+}
