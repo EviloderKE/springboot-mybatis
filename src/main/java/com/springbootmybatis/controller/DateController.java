2fd0e1405ee91292212c91ac220b78c2da0699ac
ke <447511881@qq.com>
2020-10-05 20:51:22 +0800

    dateUtil

--- src/main/java/com/springbootmybatis/controller/DateController.java
+++ src/main/java/com/springbootmybatis/controller/DateController.java
@@ -1,11 +1,13 @@
 package com.springbootmybatis.controller;
 
+import cn.hutool.core.date.DateUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 @RestController
@@ -27,6 +29,29 @@ public class DateController {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    @GetMapping("/dateutil")
+    public void dateUtil(){
+        //当前时间
+        Date date = DateUtil.date();
+        System.out.println(date);
+
+        //当前时间
+        Date date2 = DateUtil.date(Calendar.getInstance());
+        System.out.println(date2);
+
+        //当前时间
+        Date date3 = DateUtil.date(System.currentTimeMillis());
+        System.out.println(date3);
+
+        //当前时间字符串，格式：yyyy-MM-dd HH:mm:ss
+        String now = DateUtil.now();
+        System.out.println(now);
+
+        //当前日期字符串，格式：yyyy-MM-dd
+        String today= DateUtil.today();
+        System.out.println(today);
     }
 
 }
