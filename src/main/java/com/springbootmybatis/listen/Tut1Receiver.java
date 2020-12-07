0142c1ca35bfc8b088952da9c68aa955a080ca77
zk <447511881@qq.com>
2020-12-07 14:45:31 +0800

    rabbitmq 手动ack

--- src/main/java/com/springbootmybatis/listen/Tut1Receiver.java
+++ src/main/java/com/springbootmybatis/listen/Tut1Receiver.java
@@ -1,16 +1,24 @@
 package com.springbootmybatis.listen;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Profile;
+
+import java.io.IOException;
 
 @Profile("rabbitmq")
 @RabbitListener(queues = "hello")
 public class Tut1Receiver {
 
     @RabbitHandler
-    public void receive(String in) {
+    public void receive(String in, Channel channel, Message message) throws IOException {
+
         System.out.println(" [x] Received '" + in + "'");
+
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        System.out.println("消息已确认");
     }
 
 }
