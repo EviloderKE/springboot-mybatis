de1965886f5ee070d76ab74de1df0cec160a16fe
zk <447511881@qq.com>
2021-02-24 10:59:24 +0800

    spring security 配置

---
+++ src/main/java/com/springbootmybatis/config/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.springbootmybatis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Web资源权限控制
+     *
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers("/config/**", "/css/**", "/fonts/**", "/img/**", "/js/**");
+
+        //Ant Design登录页面，限定GET，避免和 Spring Security 的login（POST方式）冲突
+        web.ignoring().antMatchers(HttpMethod.GET,"/login");
+
+        //Ant Design 页面
+        web.ignoring().antMatchers("/","/console", "/console/**","/static/**","/*.png","/*.js","/*.css");
+
+        //swagger-ui start
+        web.ignoring().antMatchers("/v2/api-docs/**");
+        web.ignoring().antMatchers("/swagger.json");
+        web.ignoring().antMatchers("/swagger-ui.html");
+        web.ignoring().antMatchers("/swagger-resources/**");
+        web.ignoring().antMatchers("/webjars/**");
+        //swagger-ui end
+    }
+
+    /**
+     * HTTP请求权限控制
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/date/**").permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+}
